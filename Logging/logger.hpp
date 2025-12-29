dc69c110ce1c783d4e97fc8d5878a9084a4d77b2
vesa <vesanaumanen@gmail.com>
2025-12-29 09:03:50 +0700

    First version of header for singleton logger class.

---
+++ Logging/logger.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "ilogger.hpp"
+
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <functional>
+#include <memory>
+#include <atomic>
+
+namespace Logging
+{
+    /// @brief Singleton class for logging. This class has a buffer for messages to provide performance on burst logging situations.
+    class Logger : public ILogger
+    {
+        public:
+
+        /// @brief Construct the logger with another logger. Disallow implicit conversions.
+        /// @param logger Logger that actually logs the messages to its target location.
+        explicit Logger( std::shared_ptr<ILogger> logger );
+
+        /// @brief Destructor. Flushes the buffer and stops the processor thread.
+        virtual ~Logger();
+
+        void logError( std::string message, bool add_prefix ) override;
+        void logWarning( std::string message, bool add_prefix ) override;
+        void logInfo( std::string message, bool add_prefix ) override;
+        void logDebug( std::string message, bool add_prefix ) override;
+        void logVerbose( std::string message, bool add_prefix ) override;
+
+        // Prevent copying and moving. Otherwise threading, mutexes and such might get mixed.
+        Logger( const Logger& ) = delete;
+        Logger& operator=( const Logger& ) = delete;
+        Logger( Logger&& ) = delete;
+        Logger& operator=( Logger&& ) = delete;
+
+        private:
+
+        /// @brief Should the logger still be running.
+        std::atomic<bool> m_running{ false };
+
+        /// @brief Buffer for the messages.
+        std::queue<std::function<void()>> m_buffer;
+
+        /// @brief Mutex for modifying the buffer.
+        std::mutex m_buffer_mutex;
+
+        /// @brief Condition variable used to signal processor thread that there are items in the queue.
+        std::condition_variable m_buffer_signal;
+
+        /// @brief Thread that processes the buffer, i.e. logs the messages.
+        std::thread m_buffer_processor;
+
+        /// @brief Logger that logs the messages.
+        std::shared_ptr<ILogger> m_target_logger;
+
+        /// @brief Method for the m_buffer_processor.
+        void processBuffer();
+    };
+}
