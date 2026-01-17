69029ffdfc4b6336d7e733592e02033d87f571ce
vesa <vesanaumanen@gmail.com>
2026-01-17 15:04:00 +0700

    Added comments.

--- Configuration/configuration.hpp
+++ Configuration/configuration.hpp
@@ -6,9 +6,15 @@
 
 namespace Configuration
 {
+    /// @brief Initialize a global config.
+    /// @param config Config.
     void init( std::unique_ptr<IConfig> config );
 
+    /// @brief Shutdown the global config.
+    /// Note: NEVER call this from global constructors or destructors. Can cause hard to debug issues and crashes due to initialization order.
     void shutdown();
 
+    /// @brief Get the global config.
+    /// @return Global config.
     IConfig& get();
 }
